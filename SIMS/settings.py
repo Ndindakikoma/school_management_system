f982434001656ad5932b079d0ba918f62389c415
Ndindakikoma <ndindakikoma@gmail.com>
2022-10-17 20:43:32 +0300

    modifications

--- SIMS/settings.py
+++ SIMS/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
 
     }
 }
-
+AUTH_USER_MODEL = 'school_management_app.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
